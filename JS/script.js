fc683de73ff432a43fb57d0c9a53d8d5a9ee3db1
mylcevaleksandr <myltsevaleksandr@gmail.com>
2023-02-06 23:20:41 +0300

    Final changes for Control #7

--- JS/script.js
+++ JS/script.js
@@ -1,54 +1,69 @@
-
 // Часть 1
 
-let clients = [ {
-    firstName: "Александр",
-    lastName: "Иванчук",
-    date: "11-29-1990",
-    phone: "8 (929) 988-90-09",
-    amounts: [ 2546, 2098, 764, 7266 ]
-}, {
-    firstName: "Анатолий", lastName: "Стаценко", date: "02-12-1987", phone: null, amounts: [ 563, 8287, 889 ]
-}, {
-    firstName: "Марина",
-    lastName: "Петрова",
-    date: "02-06-1997",
-    phone: "8 (899) 546-09-08",
-    amounts: [ 6525, 837, 1283, 392 ]
-}, {
-    firstName: "Иван", lastName: "Караванов", date: "09-12-1999", phone: null, amounts: [ 7634, 283, 9823, 3902 ]
-}, {
-    firstName: "Оксана",
-    lastName: "Абрамова",
-    date: "01-24-2002",
-    phone: "8 (952) 746-99-22",
-    amounts: [ 342, 766, 362 ]
-} ];
+let clients = [
+    {
+        firstName: "Александр",
+        lastName: "Иванчук",
+        date: "11-29-1990",
+        phone: "8 (929) 988-90-09",
+        amounts: [ 2546, 2098, 764, 7266 ]
+    },
+    {
+        firstName: "Анатолий",
+        lastName: "Стаценко",
+        date: "02-12-1987",
+        phone: null,
+        amounts: [ 563, 8287, 889 ]
+    },
+    {
+        firstName: "Марина",
+        lastName: "Петрова",
+        date: "02-06-1997",
+        phone: "8 (899) 546-09-08",
+        amounts: [ 6525, 837, 1283, 392 ]
+    },
+    {
+        firstName: "Иван",
+        lastName: "Караванов",
+        date: "09-12-1999",
+        phone: null,
+        amounts: [ 7634, 283, 9823, 3902 ]
+    },
+    {
+        firstName: "Оксана",
+        lastName: "Абрамова",
+        date: "01-24-2002",
+        phone: "8 (952) 746-99-22",
+        amounts: [ 342, 766, 362 ]
+    }
+];
+
+/* Задача 1 */
 
 let newClient = {
+    amounts: []
 };
 
+/* Задача 2, 3, 4 */
 
 function fillClientData( client ) {
     client.firstName = prompt( "Please enter your first name." );
     client.lastName = prompt( "Please enter your last name." );
     client.date = new Date( prompt( "Please enter your birthday in MM.DD.YYYY." ) );
     client.phone = prompt( "Please enter your phone." );
-    let amounts = [];
-    client.amounts = amounts;
 
     function confirmation() {
         return confirm( "Should we add purchases for client: " + client.firstName + " " + client.lastName + " ?" );
     };
     while ( confirmation() == true ) {
-        amounts.push( (parseInt( prompt( "Please enter the amount." ) )) );
+        client.amounts.push( (parseInt( prompt( "Please enter the amount." ) )) );
     }
 
     clients.push( client );
 }
 
 // fillClientData( newClient );
-//
+
 // console.log( clients );
 
 /* Часть 2 */
@@ -59,7 +74,7 @@ function fullName( clientArr, clientNumber ) {
     return clientArr[clientNumber].firstName + " " + clientArr[clientNumber].lastName;
 }
 
-// console.log( "Полное имя клиента: " + fullName( clients, 5 ) );
+// console.log( "Полное имя клиента: " + fullName( clients, 0 ) );
 
 /* Задача 2 */
 
@@ -74,10 +89,10 @@ function getBirthday( birthday, date ) {
         month: "long", day: "numeric"
     } );
     if ( currentDay === bDay && currentMonth === bMonth ) {
-        console.log( "Ваш день рождения " + formatter.format( bDate ) + " " + "(Сегодня)!" );
+        return formatter.format( bDate ) + " " + "(cегодня)!";
     }
     else {
-        console.log( "Ваш день рождения " + formatter.format( bDate ) + " (не сегодня)." );
+        return formatter.format( bDate ) + ".";
     }
 }
 
@@ -94,8 +109,96 @@ function getAllAmount( arr ) {
 /*  Задача 4 */
 
 function getAverageAmount( arr ) {
-    let average = arr.reduce( ( a, b ) => a + b ) / arr.length;
-    return average.toFixed( 1 );
+    return arr.reduce( ( a, b ) => a + b ) / arr.length.toFixed( 1 );
 }
 
-// console.log( "Средний чек клиента: " + getAverageAmount( clients[0].amounts ) );
+// console.log( "Средний чек клиента: " + getAverageAmount( clients[4].amounts ) );
+
+/* Задача 5; Все временные вызовы функций не удалил, а закомментировал. */
+
+/* Задача 6 */
+
+let showClients = ( arr ) => {
+    for ( let i = 0; i < arr.length; i++ ) {
+        console.log( "Клиент, " + fullName( arr, [ i ] ) + ", имеет среднюю сумму чека: " + getAverageAmount( arr[i].amounts ) + " . День рождения клиента: " + getBirthday( arr[i].date, new Date() ) );
+    }
+};
+
+/* Задача 7 */
+// showClients( clients );
+
+/* Задача 8 */
+
+try {
+    showClients();
+} catch ( error ) {
+    console.log( "Вызвана функция без параметров." );
+}
+
+/* задача 9*/
+
+let bestClients = [
+    {
+        firstName: "Михаил",
+        lastName: "Михеев",
+        date: "10-30-1991",
+        phone: "8 (929) 988-90-09",
+        amounts: [ 254, 209, 76, 726 ]
+    },
+    {
+        firstName: "Александр",
+        lastName: "Бонд",
+        date: "01-10-1980",
+        phone: null,
+        amounts: [ 56, 828, 88, 259 ]
+    },
+    {
+        firstName: "Алина",
+        lastName: "Макарова",
+        date: "03-07-1995",
+        phone: "8 (899) 546-09-08",
+        amounts: [ 652, 83, 128, 92, 1034, 546 ]
+    },
+    {
+        firstName: "Анна",
+        lastName: "Каракай",
+        date: "03-02-1969",
+        phone: null,
+        amounts: [ 734, 28, 923, 390 ]
+    },
+    {
+        firstName: "Ольга",
+        lastName: "Нахимова",
+        date: "11-25-2006",
+        phone: "8 (952) 746-99-22",
+        amounts: [ 32, 76, 36, 567 ]
+    }
+];
+
+// showClients( bestClients );
+
+
+/* Задача 10 */
+
+setTimeout( () => {
+    showClients( bestClients );
+}, 3000 );
+
+/* Задача 11 */
+
+function whoSpentMore( arr ) {
+    let biggestSum = 0;
+    let richestClient = "";
+    for ( let i = 0; i < arr.length; i++ ) {
+        if ( biggestSum < getAllAmount( arr[i].amounts ) ) {
+            biggestSum = getAllAmount( arr[i].amounts );
+            richestClient = fullName( arr, [ i ] );
+        }
+    }
+    console.log( "Больше всех потратил клиент: " + richestClient + ". Сумма покупок составила " + biggestSum + "." );
+}
+
+whoSpentMore( bestClients );
+
+/* Задача 12 */
+/* Вроде все работает ;-) */
